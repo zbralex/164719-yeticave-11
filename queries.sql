22e2e88b3ce1bfdbc37d308ef3eaf063c998a446
Александр Зубрицкий <5998971@gmail.com>
2020-01-24 12:24:18 +0300

    добавляет запрос для заполнения категорий

---
+++ queries.sql
@@ -0,0 +1,2 @@
+INSERT INTO yeticave.categories (name, symbol_code)
+VALUES ('Доски и лыжи', 'boards_and_skis'), ('Крепления', 'bindings'), ('Ботинки', 'boots'), ('Одежда', 'clothes'), ('Инструменты', 'tools'), ('Разное', 'other');
